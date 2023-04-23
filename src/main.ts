16782f673797a29be5d5c3fe84ee61f5bc1b68cd
D'Arcy Rittich <drittich@gmail.com>
2023-04-23 18:44:13 -0400

    WIP

--- src/main.ts
+++ src/main.ts
@@ -15,21 +15,30 @@ function generateRandomRhythm(canvas: HTMLCanvasElement) {
 	stave.setContext(ctx).draw();
 
 	const noteDurations: string[] = ['w', 'h', 'q', '8', '16'];
-	const durationValues: { [key: string]: number } = { w: 4, h: 2, q: 1, '8': 0.5, '16': 0.25 }; // Fix the object syntax
+	const durationValues: { [key: string]: number } = { w: 4, h: 2, q: 1, '8': 0.5, '16': 0.25 };
 	const numBeats = 4;
 	let remainingBeats = numBeats;
-	const notes: any = []; // Define notes type
+	const notes: any = [];
+	const beams: any = [];
 
 	while (remainingBeats > 0) {
 		const duration: string = noteDurations[Math.floor(Math.random() * noteDurations.length)];
 
 		if (durationValues[duration] <= remainingBeats) {
-			const note: any = new Vex.Flow.StaveNote({
-				keys: ['c/4'], // You can customize the pitch of the notes here
+			const note = new Vex.Flow.StaveNote({
+				keys: ['c/4'],
 				duration: duration,
 			});
 			notes.push(note);
 			remainingBeats -= durationValues[duration];
+		}
+	}
+
+	// Create beams for consecutive 16th and 8th notes
+	for (let i = 0; i < notes.length - 1; i++) {
+		if ((notes[i].duration === '16' && notes[i + 1].duration === '16') || (notes[i].duration === '8' && notes[i + 1].duration === '8')) {
+			const beam = new Vex.Flow.Beam([notes[i], notes[i + 1]]);
+			beams.push(beam);
 		}
 	}
 
@@ -38,4 +47,7 @@ function generateRandomRhythm(canvas: HTMLCanvasElement) {
 
 	const formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 800);
 	voice.draw(ctx, stave);
+
+	// Draw beams
+	beams.forEach((beam: any) => beam.setContext(ctx).draw());
 }
